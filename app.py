efeb0bd65299235a312e9b0e3cffe269b6d772c5
Trác Huỳnh <trachuynh2014@gmail.com>
2021-04-11 20:17:48 -0400

    add coverage run

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+import unittest
+import helloworld
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(helloworld.testing('a'), 'a')
+
 
 app = Flask(__name__)
 
@@ -10,3 +18,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
+    unittest.main()
---
+++ helloworld.py
@@ -0,0 +1,2 @@
+def testing(var):
+    return var
